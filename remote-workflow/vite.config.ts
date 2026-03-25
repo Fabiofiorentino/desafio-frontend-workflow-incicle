505b3b3379c85d35cd44fa6858b33312cf0b6b76
Fabiofiorentino <fabiovilaas@gmail.com>
2026-03-25 16:08:43 -0300

    docs: atualizar README e configurar porta do remote

--- remote-workflow/vite.config.ts
+++ remote-workflow/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { env } from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: Number(env.VITE_APP_PORT) || 5174,
+    strictPort: true,
+  },
 })
